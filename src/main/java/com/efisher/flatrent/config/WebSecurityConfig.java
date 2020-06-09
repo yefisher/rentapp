78e0aa2e302324df2b9904ff7ddedfe0a65380f0
Yegor Fisher <egorfisher88@gmail.com>
2020-06-09 14:54:09 +0300

    Configured security layer

--- src/main/java/com/efisher/flatrent/config/WebSecurityConfig.java
+++ src/main/java/com/efisher/flatrent/config/WebSecurityConfig.java
@@ -4,17 +4,60 @@ import com.efisher.flatrent.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/webapp/**");
+    }
+
+
     //todo: configure security module
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login*", "/logout*", "/api/flat-rent-app/user/register").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .sessionManagement()
+                .invalidSessionUrl("/invalidSession")
+                .maximumSessions(1).sessionRegistry(sessionRegistry())
+                .and().sessionFixation().none()
+                .and()
+                .logout()
+                .invalidateHttpSession(false)
+                .logoutSuccessUrl("/logout?logoutSucc=true")
+                .deleteCookies("JSESSIONID")
+                .permitAll();
+
+    }
+
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

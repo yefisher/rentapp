77cdf8dd6e3671b9b8d664b1baa3ba6ae56d3160
Yegor Fisher <egorfisher88@gmail.com>
2020-06-13 20:20:00 +0300

    code clean up

--- src/main/java/com/efisher/flatrent/controller/UserController.java
+++ src/main/java/com/efisher/flatrent/controller/UserController.java
@@ -5,7 +5,7 @@ import com.efisher.flatrent.dto.UserDTO;
 import com.efisher.flatrent.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,19 +16,30 @@ public class UserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    private final MongoTemplate mongoTemplate;
+
+    public UserController(UserService userService, MongoTemplate mongoTemplate) {
+        this.userService = userService;
+        this.mongoTemplate = mongoTemplate;
+    }
 
     @PostMapping(value = "/registration")
     public ResponseEntity<User> registerNewUserAccount(@RequestBody UserDTO userDTO) {
 
-        LOGGER.debug("Performing new user registration with info {}", userDTO);
+        LOGGER.info("Performing new user registration with info {}", userDTO);
         User user = null;
         try {
             user = userService.registerNewUserAccount(userDTO);
         } catch (RuntimeException e) {
             LOGGER.error(e.toString());
+            return ResponseEntity.badRequest().build();
         }
+
+        LOGGER.info("New user {} has been registered", userDTO);
+
+        assert user != null;
         return ResponseEntity.ok(user);
     }
 }

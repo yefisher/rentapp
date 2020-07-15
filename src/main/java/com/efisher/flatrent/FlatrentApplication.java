f11d155e02ab453246fb98db7cb6374d006282cc
Yegor Fisher <egorfisher1988@gmail.com>
2020-07-15 12:21:34 +0300

    fixed code

--- src/main/java/com/efisher/flatrent/FlatrentApplication.java
+++ src/main/java/com/efisher/flatrent/FlatrentApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlatRentApplication {
+public class FlatrentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlatRentApplication.class, args);
+        SpringApplication.run(FlatrentApplication.class, args);
     }
 }
